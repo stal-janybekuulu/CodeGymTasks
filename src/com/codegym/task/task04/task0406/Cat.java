e5d1db869e4a035989ac428ddad9e8d2bdeb834c
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-21 23:33:15 +0600

    4.02.01

---
+++ src/com/codegym/task/task04/task0406/Cat.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task04.task0406;
+
+/* 
+Name register
+
+*/
+
+public class Cat {
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+
+        this.fullName = fullName;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Vasya", "Popov");
+        System.out.println(cat.fullName);
+    }
+}
