05c8c36aa65975f2e1ac598f8871d35748bf66b0
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-21 23:22:02 +0600

    4.02.01

---
+++ src/com/codegym/task/task04/task0405/Cat.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task04.task0405;
+
+/* 
+Setting the number of cats
+
+*/
+
+public class Cat {
+    private static int catCount = 0;
+
+    public static void setCatCount(int catCount) {
+        Cat.catCount = catCount;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Cat.setCatCount(2);
+        System.out.println(catCount);
+    }
+}
