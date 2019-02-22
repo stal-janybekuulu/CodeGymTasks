715bd7e9951d569357602c7824c9fc37da5ae2b9
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-23 00:53:56 +0600

    Number of days in the year

---
+++ src/com/codegym/task/task04/task0414/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task04.task0414;
+
+/* 
+Number of days in the year
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 400 == 0) {
+            year = 366;
+            System.out.println("Number of days in the year: " + year);
+        }
+
+        else {
+            year = 365;
+            System.out.println("Number of days in the year: " + year);
+        }
+    }
+}
