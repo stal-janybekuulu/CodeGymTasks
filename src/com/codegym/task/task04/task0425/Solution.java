426c5d6b213c8cbdb70ab996d9a5114c285f2e7a
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-24 22:36:18 +0600

    Target locked!

---
+++ src/com/codegym/task/task04/task0425/Solution.java
@@ -0,0 +1,30 @@
+package com.codegym.task.task04.task0425;
+
+/* 
+Target locked!
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println(2);
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println(3);
+        }
+        else if(x > 0 && y < 0) {
+            System.out.println(4);
+        }
+    }
+}
