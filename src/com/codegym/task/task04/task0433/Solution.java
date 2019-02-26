a3253793e62e5e643bdb3daf1211cad6b0c10a8c
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-26 22:36:16 +0600

    Seeing dollars in your future

---
+++ src/com/codegym/task/task04/task0433/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task04.task0433;
+
+
+/* 
+Seeing dollars in your future
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int i = 10;
+        String dollars = "$$$$$$$$$$";
+        while (i > 0) {
+            System.out.println(dollars);
+            i -= 1;
+        }
+
+    }
+}
