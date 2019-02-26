ce21e20b167ef0dc127bcda9786ddbb2084a7c88
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-26 23:52:35 +0600

    Even numbers

---
+++ src/com/codegym/task/task04/task0435/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task04.task0435;
+
+/* 
+Even numbers
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int i=2; i<=100; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+}
