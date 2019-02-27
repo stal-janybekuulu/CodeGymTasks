ae9745bbde6779b0edf49190072442d38bad28a9
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-27 22:52:24 +0600

    Drawing lines

---
+++ src/com/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task04.task0438;
+
+/* 
+Drawing lines
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int i=1; i<=10; i++) {
+            System.out.print(8);
+        }
+        for (int j=1; j<=10; j++) {
+            System.out.println(8);
+        }
+    }
+}
