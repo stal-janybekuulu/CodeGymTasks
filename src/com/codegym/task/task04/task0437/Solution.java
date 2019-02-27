df83bfb7fda5949a0e6bb1162992e9f120598d5d
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-02-27 22:15:34 +0600

    Triangle of eights

---
+++ src/com/codegym/task/task04/task0437/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task04.task0437;
+
+
+/* 
+Triangle of eights
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int i=1; i<=10; i++) {
+            for (int j=1; j<=i; j++){
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}
