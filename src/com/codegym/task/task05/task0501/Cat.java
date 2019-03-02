1d9fa4ef40b159b99bfe70ba43ced50fd1f2c3b0
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-02 11:50:23 +0600

    Creating a cat

---
+++ src/com/codegym/task/task05/task0501/Cat.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task05.task0501;
+
+/* 
+Creating a cat
+
+*/
+
+public class Cat {
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+
+    }
+}
