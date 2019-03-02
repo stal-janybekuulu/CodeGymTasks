1c33ee3b04efc410088439e38efef85df59d4cb4
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-02 12:23:05 +0600

    Feline carnage

---
+++ src/com/codegym/task/task05/task0505/Solution.java
@@ -0,0 +1,42 @@
+package com.codegym.task.task05.task0505;
+
+/* 
+Feline carnage
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("cat1", 2, 3, 4);
+        Cat cat2 = new Cat("cat2", 3,4,5);
+        Cat cat3 = new Cat("cat3", 4,5,6);
+
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat1.fight(cat3));
+        System.out.println(cat2.fight(cat3));
+
+    }
+
+    public static class Cat {
+        protected String name;
+        protected int age;
+        protected int weight;
+        protected int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            int ageAdvantage = this.age > anotherCat.age ? 1 : 0;
+            int weightAdvantage = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthAdvantage = this.strength > anotherCat.strength ? 1 : 0;
+
+            int score = ageAdvantage + weightAdvantage + strengthAdvantage;
+            return score > 2; // return score > 2 ? true : false;
+        }
+    }
+}
