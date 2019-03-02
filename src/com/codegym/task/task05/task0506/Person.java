035a6456f6715f403e7fbe9b2054e05672158a77
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-02 12:24:34 +0600

    People

---
+++ src/com/codegym/task/task05/task0506/Person.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task05.task0506;
+
+/* 
+People
+
+*/
+
+public class Person {
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
