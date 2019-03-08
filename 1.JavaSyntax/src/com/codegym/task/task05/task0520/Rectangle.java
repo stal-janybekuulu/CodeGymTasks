2a71d16492321eb9059804ea22c69bf0942ca9c2
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-09 03:06:07 +0600

    Create a Rectangle class

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0520/Rectangle.java
@@ -0,0 +1,48 @@
+package com.codegym.task.task05.task0520;
+
+/* 
+Create a Rectangle class
+
+*/
+
+
+public class Rectangle {
+    int top, left, width, height;
+
+    public Rectangle(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public Rectangle(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public Rectangle(int top, int left) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+    }
+
+
+    public Rectangle(int top) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
