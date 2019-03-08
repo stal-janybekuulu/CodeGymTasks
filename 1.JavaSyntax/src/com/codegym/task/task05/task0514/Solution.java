140ccc98cff364af83620480f3f00c14e2032253
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-09 01:51:02 +0600

    A programmer creates a person

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0514/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task05.task0514;
+
+/* 
+A programmer creates a person
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.initialize("John", 20);
+    }
+
+    static class Person {
+        String name;
+        int age;
+        public void initialize (String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
