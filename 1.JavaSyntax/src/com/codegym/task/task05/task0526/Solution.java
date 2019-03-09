dc50916050b1a868c63d209cb7e81a5b991f6dcc
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-09 14:40:27 +0600

    Man and woman

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0526/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task05.task0526;
+
+/* 
+Man and woman
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("John", 22, "Calinina 15");
+        Man man2 = new Man("Mike", 25, "Pushkina 23");
+
+        Woman woman1 = new Woman("Ulya", 20, "Lenina 12");
+        Woman woman2 = new Woman("Olga", 35, "Minina 43");
+
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+    }
+
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        public Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        public Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
