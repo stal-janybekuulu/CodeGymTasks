b330238a434b7e6631e34ea67c2ed970383a7203
stal-janybekuulu <stal.janybekuulu@gmail.com>
2019-03-09 19:24:09 +0600

    Boss, something weird is happening

---
+++ 1.JavaSyntax/src/com/codegym/task/task05/task0530/Solution.java
@@ -0,0 +1,27 @@
+package com.codegym.task.task05.task0530;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Boss, something weird is happening
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int a = reader.readLine();
+//        int b = reader.read();
+//
+//        int sum = a + b;
+//        System.out.println("Sum = " + sum);
+    }
+}
