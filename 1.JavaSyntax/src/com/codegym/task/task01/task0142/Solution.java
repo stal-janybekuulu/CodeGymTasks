638c2e6775c8de9a71a6498122e3ca7b591d3ecc
Stal <stal.janybekuulu@gmail.com>
2019-03-13 19:24:46 +0600

    Amigo is very smart

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0142/Solution.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task01.task0142;
+
+/* 
+Amigo is very smart
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Amigo is very smart");
+    }
+}
