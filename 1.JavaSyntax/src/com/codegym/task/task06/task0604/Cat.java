76b55ff8de8bb5e0215c8c55b0fab4e9db4d6c7c
Stal <stal.janybekuulu@gmail.com>
2019-03-14 11:50:34 +0600

    Cat counter

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0604/Cat.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task06.task0604;
+
+/* 
+Cat counter
+
+*/
+
+public class Cat {
+    public static int catCount = 0;
+
+    public Cat() {
+        Cat.catCount++;
+    }
+
+    protected void finalize() throws Throwable {
+        Cat.catCount--;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
