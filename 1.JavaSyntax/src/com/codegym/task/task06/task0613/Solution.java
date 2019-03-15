6368eca97839ed2dc338186689acb93e613dde22
Stal <stal.janybekuulu@gmail.com>
2019-03-15 12:09:25 +0600

    Cat and statics

---
+++ 1.JavaSyntax/src/com/codegym/task/task06/task0613/Solution.java
@@ -0,0 +1,24 @@
+package com.codegym.task.task06.task0613;
+
+/* 
+Cat and statics
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            Cat cat = new Cat();
+        }
+
+        System.out.println(Cat.catCount);
+    }
+
+    public static class Cat {
+        public static int catCount;
+
+        public Cat() {
+            Cat.catCount++;
+        }
+    }
+}
