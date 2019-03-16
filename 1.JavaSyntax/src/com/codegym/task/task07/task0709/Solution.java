aa4c02cff49f63ba9541b54e8ec2f85160add3bc
Stal <stal.janybekuulu@gmail.com>
2019-03-17 02:50:25 +0600

    Expressing ourselves more concisely

---
+++ 1.JavaSyntax/src/com/codegym/task/task07/task0709/Solution.java
@@ -0,0 +1,40 @@
+package com.codegym.task.task07.task0709;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* 
+Expressing ourselves more concisely
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i=0; i<5; i++) {
+            String text = scanner.nextLine();
+            list.add(text);
+        }
+
+        String shortest = list.get(0);
+        String h = "";
+        for (String st : list) {
+            if (st.length() < shortest.length()) {
+                shortest = st;
+            }
+            if (st.length() == shortest.length()) {
+                h = st;
+            }
+        }
+
+        for (int i=0; i<5; i++) {
+            if (shortest.length() == list.get(i).length()) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
