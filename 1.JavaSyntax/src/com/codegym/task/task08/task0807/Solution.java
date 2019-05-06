862651d17462b295c649c71b28da8e4c148782e6
stal <stal.janybekuulu@gmail.com>
2019-05-06 18:58:06 +0600

    LinkedList and ArrayList

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0807/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList and ArrayList
+
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //write your code here
+        ArrayList<Object> list = new ArrayList<>();
+        return list;
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+        LinkedList<Object> objects = new LinkedList<>();
+        return objects;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
