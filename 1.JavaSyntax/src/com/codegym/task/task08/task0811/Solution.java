3be7450bb117c5a4bb29dd1b86cd840d2c65c12f
stal <stal.janybekuulu@gmail.com>
2019-05-06 20:56:58 +0600

    Method quartet

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0811/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Method quartet
+
+*/
+
+public class Solution {
+    public static List getListForGet() {
+        //write your code here
+        ArrayList<Object> list = new ArrayList<>();
+        return list;
+    }
+
+    public static List getListForSet() {
+        //write your code here
+        ArrayList<Object> list = new ArrayList<>();
+        return list;
+    }
+
+    public static List getListForAddOrInsert() {
+        //write your code here
+        LinkedList<Object> objects = new LinkedList<>();
+        return objects;
+    }
+
+    public static List getListForRemove() {
+        //write your code here
+        LinkedList<Object> objects = new LinkedList<>();
+        return objects;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
