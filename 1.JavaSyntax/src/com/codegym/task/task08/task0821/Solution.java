0486e42c2db34a0306dad05056ce28c763694e0c
stal <stal.janybekuulu@gmail.com>
2019-05-09 03:16:47 +0600

    Shared last names and first names

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0821/Solution.java
@@ -0,0 +1,31 @@
+package com.codegym.task.task08.task0821;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Shared last names and first names
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleList();
+        printPeopleList(map);
+    }
+
+    public static Map<String, String> createPeopleList() {
+        //write your code here
+        HashMap<String, String> list = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            list.put("lastName", "firstName");
+        }
+        return list;
+    }
+
+    public static void printPeopleList(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
