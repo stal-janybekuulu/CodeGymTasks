613be703c7dacccb30aa850ab6e79924559f14ec
stal <stal.janybekuulu@gmail.com>
2019-05-21 01:53:08 +0600

    Let's make the code do something useful!

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0929/Solution.java
@@ -0,0 +1,49 @@
+package com.codegym.task.task09.task0929;
+
+import java.io.*;
+
+/* 
+Let's make the code do something useful!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sourceFileName = reader.readLine();
+        InputStream fileInputStream = null;
+
+        int repeat = 1;
+        while (repeat >= 0) {
+            try {
+                if (repeat == 0) sourceFileName = reader.readLine();
+                fileInputStream = getInputStream(sourceFileName);
+                repeat = -1;
+            } catch (FileNotFoundException e) {
+                System.out.println("File does not exist.");
+                repeat --;
+            }
+         }
+
+        String destinationFileName = reader.readLine();
+        OutputStream fileOutputStream = getOutputStream(destinationFileName);
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+
+    public static InputStream getInputStream(String fileName) throws IOException {
+        return new FileInputStream(fileName);
+    }
+
+    public static OutputStream getOutputStream(String fileName) throws IOException {
+        return new FileOutputStream(fileName);
+    }
+}
+
