1583d1e388a95ef2c3179f2fdb87ab5abc17ffc5
stal <stal.janybekuulu@gmail.com>
2019-06-23 15:11:07 +0600

    Three methods and a minimum

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1209/Solution.java
@@ -0,0 +1,38 @@
+package com.codegym.task.task12.task1209;
+
+/* 
+Three methods and a minimum
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static int min(int x, int y) {
+        if (x > y) {
+            return y;
+        } else {
+            return x;
+        }
+    }
+
+    public static long min(long x, long y) {
+        if (x > y) {
+            return y;
+        } else {
+            return x;
+        }
+    }
+
+    public static double min(double x, double y) {
+        if (x > y) {
+            return y;
+        } else {
+            return x;
+        }
+    }
+
+}
