af796e5626b9865c9b47c390f766798110693c84
stal <stal.janybekuulu@gmail.com>
2019-06-23 16:31:44 +0600

    Fly, run and swim

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1216/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task12.task1216;
+
+/* 
+Wanna fly?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add an interface here
+    public interface CanFly {
+        static void method1() { }
+
+        static void methdo2() { }
+    }
+
+}
---
+++ 2.JavaCore/src/com/codegym/task/task12/task1217/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task12.task1217;
+
+/* 
+Fly, run and swim
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add interfaces here
+    public interface CanFly {
+        static void fly() {}
+    }
+
+    public interface CanRun {
+        static void run() { }
+    }
+
+    public interface CanSwim {
+        static void swim() { }
+    }
+
+}
