27c175d9f5c6cdafdab020bf3c7c8f9502f580ba
stal <stal.janybekuulu@gmail.com>
2019-06-23 13:59:10 +0600

    Freedom of the press

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print() {}
+    public static void print(int x) {}
+    public static void print(int x, int y) {}
+    public static void print(String x) {}
+    public static void print(String x, Integer y) {}
+}
