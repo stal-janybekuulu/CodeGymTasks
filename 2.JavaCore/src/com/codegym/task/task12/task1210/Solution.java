d1e68d010d91339f2fa3ac7c8ea98292f63e2ffe
stal <stal.janybekuulu@gmail.com>
2019-06-23 15:14:00 +0600

    Three methods and a maximum

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1210/Solution.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task12.task1210;
+
+/* 
+Three methods and a maximum
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static long max(long x, long y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static double max(double x, double y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+}
