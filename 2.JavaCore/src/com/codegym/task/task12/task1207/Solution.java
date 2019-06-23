69f8f03a41cb16dc4f156422dc68e54c9a66d300
stal <stal.janybekuulu@gmail.com>
2019-06-23 13:57:04 +0600

    int and Integer

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int x = 1;
+        Integer y = 2;
+
+        print(x);
+        print(y);
+
+    }
+
+    //write your code here
+    public static void print(int x) { }
+    public static void print(Integer x) { }
+}
