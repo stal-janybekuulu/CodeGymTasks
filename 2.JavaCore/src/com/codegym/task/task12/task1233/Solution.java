de325ade4860479ecbfdf36dffcbd005ba30b6d1
stal <stal.janybekuulu@gmail.com>
2019-08-02 00:36:05 +0600

    The isomorphs are coming

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1233/Solution.java
@@ -0,0 +1,54 @@
+package com.codegym.task.task12.task1233;
+
+/* 
+The isomorphs are coming
+
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The index of the minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //write your code here
+        int indexMax = 0;
+        int indexMin = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[indexMax]) {
+                indexMax = i;
+            }
+            else if (array[i] < array[indexMin]) {
+                indexMin = i;
+            }
+        }
+
+        Arrays.sort(array);
+        int minNumber = array[0];
+
+        return new Pair<Integer, Integer>(minNumber, indexMin);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
