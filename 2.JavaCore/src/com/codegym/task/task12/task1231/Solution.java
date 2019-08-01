f224f167d62a4a5cf5a9e342ecb0e3bbd72c884a
stal <stal.janybekuulu@gmail.com>
2019-08-02 00:23:09 +0600

    Useless abstractions

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1231/Solution.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task12.task1231;
+
+/* 
+Useless abstractions
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Horse horse = new Pegasus();
+        horse.run();
+
+        Horse horse1 = new Horse();
+    }
+
+    public static interface CanFly {
+        public abstract void fly();
+    }
+
+    public static class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus extends Horse implements CanFly {
+        public void fly() {
+
+        }
+    }
+
+    public abstract static class SwimmingPegasus extends Pegasus {
+        public abstract void swim();
+    }
+
+}
