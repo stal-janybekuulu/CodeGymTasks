5e0924d33c10b6beccd39e83f1e9ecab2f8d00c1
github <stal.janybekuulu@gmail.com>
2020-04-20 18:05:33 +0600

    init

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1319/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task13.task1319;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+/* 
+Writing to a file from the console
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
