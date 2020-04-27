efb71fac6059f8062b3636429085598d4f25d0d2
github <stal.janybekuulu@gmail.com>
2020-04-27 15:18:10 +0600

    Writing to a file from the console

--- 2.JavaCore/src/com/codegym/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task13/task1319/Solution.java
@@ -13,5 +13,23 @@ Writing to a file from the console
 public class Solution {
     public static void main(String[] args) {
         // write your code here
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()));
+
+            while (true) {
+                String s = reader.readLine();
+                if (s.equals("exit")) {
+                    break;
+                } else {
+                    writer.write(s);
+                    writer.newLine();
+                }
+            }
+            writer.write("exit");
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+        }
     }
 }
